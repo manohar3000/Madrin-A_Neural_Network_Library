c76320e5e7b5b5849d3a1d924b07c8bbd3b7b250
manohar3000 <141481574+manohar3000@users.noreply.github.com>
2024-06-03 06:02:55 -0400

    Update the version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="madrin",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     install_requires=['numpy'],
     long_description=description,
