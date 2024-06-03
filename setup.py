84968de713b3125a00ff14dbb9afbc0701f345ed
manohar3000 <141481574+manohar3000@users.noreply.github.com>
2024-06-03 07:19:09 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,22 @@ with open("README.md", "r") as f:
 setup(
     name="madrin",
     version="0.0.2",
+    description="A Neural Network Library",
     packages=find_packages(),
     install_requires=['numpy'],
     long_description=description,
     long_description_content_type="text/markdown",
+    url="https://github.com/manohar3000/Madrin-A_Neural_Network_Library",
+    author="Manohar",
+    author_email="manohargehlot3000@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+    ],
 )
