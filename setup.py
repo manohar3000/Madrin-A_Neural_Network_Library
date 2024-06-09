ca18bd0bc97dc63c76658af2d3f14be135319dea
manohar3000 <141481574+manohar3000@users.noreply.github.com>
2024-06-09 05:55:15 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="madrin",
-    version="0.0.2",
+    version="1.0.0",
     description="A Neural Network Library",
     packages=find_packages(),
     install_requires=['numpy'],
